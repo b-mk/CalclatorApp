458c6fa39b7105801236dfbcef216913c171ea54
b-mk <youremailexample.com(mi161332@edu.okinawa-ct.ac.jp)>
2019-07-22 09:11:12 +0900

    プロジェクトを生やした

---
+++ CalculatorApp.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class CalculatorApp extends Frame implements ActionListener {
+    public static void main(String[] args) {
+        
+    }
+} 
